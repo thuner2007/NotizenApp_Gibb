23c8ffbff32b407609fcdb3ac3b11ae36d5d6a30
thuner2007 <thuner2007@outlook.com>
2024-12-05 10:55:12 +0100

    ugly app

--- NotizenApp/MainPage.xaml.cs
+++ NotizenApp/MainPage.xaml.cs
@@ -2,23 +2,12 @@
 {
     public partial class MainPage : ContentPage
     {
-        int count = 0;
-
+        string name = "Colin";
         public MainPage()
         {
             InitializeComponent();
-        }
 
-        private void OnCounterClicked(object sender, EventArgs e)
-        {
-            count++;
-
-            if (count == 1)
-                CounterBtn.Text = $"Clicked {count} time";
-            else
-                CounterBtn.Text = $"Clicked {count} times";
-
-            SemanticScreenReader.Announce(CounterBtn.Text);
+            helloLabel.Text = "Hello, " + name;
         }
     }
 
