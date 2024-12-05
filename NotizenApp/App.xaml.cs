ce5e07141361eb262c60a5f5ae976e35bfe96eca
vmadmin <vmadmin@WIV11-VMWP>
2024-12-05 09:04:33 +0100

    Add project files.

---
+++ NotizenApp/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace NotizenApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ NotizenApp/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace NotizenApp
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
